1fa4e267de5c36e8e38e879500b168b371e8e689
Gopalbhalani <gopalbhalani137@gmail.com>
2024-11-24 14:41:40 +0530

    Added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -61,7 +61,7 @@ function Modal({ setPredictionResult }) {
       return;
     }
     try {
-      const response = await fetch('http://localhost:3000/predict', {
+      const response = await fetch('https://heart-disease-prediction-b.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
